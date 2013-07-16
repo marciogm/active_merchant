c5d6b0f75c9f692e0b1c1eacd3e9c5ffd9236f15
marciogm <marciogiaxa@gmail.com>
2013-07-16 14:25:48 -0300

    successful purchase and unsuccessful purchase

--- lib/active_merchant/billing/gateways/two_pay.rb
+++ lib/active_merchant/billing/gateways/two_pay.rb
@@ -1,8 +1,8 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class TwoPayGateway < Gateway
-      self.test_url = 'http://twopay-api'
-      self.live_url = 'http://twopay-api'
+      self.test_url = 'https://twopay-api'
+      self.live_url = 'https://twopay-api'
 
       # The countries the gateway supports merchants from as 2 digit ISO country codes
       self.supported_countries = ['BR']
@@ -35,6 +35,7 @@ module ActiveMerchant #:nodoc:
 
       def purchase(money, creditcard, options = {})
         post = {}
+        post[:amount] = amount(money).to_i
         add_invoice(post, options)
         add_creditcard(post, creditcard)
         add_address(post, creditcard, options)
@@ -44,6 +45,7 @@ module ActiveMerchant #:nodoc:
       end
 
       def capture(money, authorization, options = {})
+        post = {}
         commit('capture', money, post)
       end
 
@@ -66,21 +68,56 @@ module ActiveMerchant #:nodoc:
       end
 
       def add_creditcard(post, creditcard)
-        post[:card_num]   = creditcard.number
-        post[:card_code]  = creditcard.verification_value if creditcard.verification_value?
-        post[:exp_date]   = expdate(creditcard)
-        post[:first_name] = creditcard.first_name
-        post[:last_name]  = creditcard.last_name
+        post["card[number]"]   = creditcard.number
+        post["card[cvc]"]  = creditcard.verification_value if creditcard.verification_value?
+        post["card[exp_month"]   = "12"
+        post["card[exp_year"]   = expdate(creditcard)
+        post["card[holdername"] = creditcard.first_name
       end
 
-      def parse(body)
+       def parse(body) 
+        fields = JSON.parse(body)
+        error = fields[0]
+
+        results = {
+          :response_code => error["errorCode"],
+          :description => error["description"],
+          :errorField => error["errorField"], 
+          :severityCodeEnum => error["severityCodeEnum"],
+          :order_status => error["order_status"]
+        }
+        results
       end
 
       def commit(action, money, parameters)
+         parameters[:paymentmethodcode] = 1
+
          data = ssl_post(self.test_url, post_data(action, parameters))
+
+         response          = parse(data)
+         response[:action] = action
+
+        message = message_from(response)
+
+        p success?(response)
+
+        Response.new(success?(response), message, response)
+        #   :authorization => response["authorization_code"],
+        #   :avs_result => { :code => response["avs_result_code"] },
+        #   :cvv_result => response["card_code"]
+        # )
+      end
+
+      def success?(response)
+        response[:order_status] == "Paid"
       end
 
       def message_from(response)
+        if response[:order_status] == "Paid"
+          return "This transaction has been approved"
+        else
+          return "This transaction has not been approved"
+        end
       end
 
       def post_data(action, parameters = {})
@@ -89,7 +126,7 @@ module ActiveMerchant #:nodoc:
         post[:type]           = action
         post[:solution_ID]    = application_id if application_id.present? && application_id != "ActiveMerchant"
 
-        request = post.merge(parameters).collect { |key, value| "x_#{key}=#{CGI.escape(value.to_s)}" }.join("&")
+        request = post.merge(parameters).collect { |key, value| "#{key}=#{CGI.escape(value.to_s)}" }.join("&")
         request
       end
     end
--- test/remote/gateways/remote_two_pay_test.rb
+++ test/remote/gateways/remote_two_pay_test.rb
@@ -4,11 +4,24 @@ class RemoteTwoPayTest < Test::Unit::TestCase
 
 
   def setup
-    @gateway = TwoPayGateway.new(fixtures(:two_pay))
+    @gateway = TwoPayGateway.new
 
-    @amount = 100
-    @credit_card = credit_card('4000100011112224')
-    @declined_card = credit_card('4000300011112220')
+    @credit_card = ActiveMerchant::Billing::CreditCard.new(
+    :number     => '3400 0000 0000 009',
+    :month      => '12',
+    :year       => '2020',
+    :first_name => 'Plano Bê',
+    :verification_value  => '0000'
+  )
+
+    @amount = 400
+    @declined_card = ActiveMerchant::Billing::CreditCard.new(
+    :number     => '3400 0000 0000 009',
+    :month      => '12',
+    :year       => '2020',
+    :first_name => 'Plano Bê',
+    :verification_value  => '000'
+  )
 
     @options = {
       :order_id => '1',
@@ -20,38 +33,20 @@ class RemoteTwoPayTest < Test::Unit::TestCase
   def test_successful_purchase
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success response
-    assert_equal 'REPLACE WITH SUCCESS MESSAGE', response.message
+    assert_equal 'This transaction has been approved', response.message
   end
 
   def test_unsuccessful_purchase
     assert response = @gateway.purchase(@amount, @declined_card, @options)
     assert_failure response
-    assert_equal 'REPLACE WITH FAILED PURCHASE MESSAGE', response.message
+    assert_equal 'This transaction has not been approved', response.message
   end
 
-  def test_authorize_and_capture
+  def test_authorize
     amount = @amount
     assert auth = @gateway.authorize(amount, @credit_card, @options)
     assert_success auth
     assert_equal 'Success', auth.message
     assert auth.authorization
-    assert capture = @gateway.capture(amount, auth.authorization)
-    assert_success capture
-  end
-
-  def test_failed_capture
-    assert response = @gateway.capture(@amount, '')
-    assert_failure response
-    assert_equal 'REPLACE WITH GATEWAY FAILURE MESSAGE', response.message
-  end
-
-  def test_invalid_login
-    gateway = TwoPayGateway.new(
-                :login => '',
-                :password => ''
-              )
-    assert response = gateway.purchase(@amount, @credit_card, @options)
-    assert_failure response
-    assert_equal 'REPLACE WITH FAILURE MESSAGE', response.message
   end
 end
